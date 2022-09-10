574aced16528348be73a218bce73bf458b87919f
Jesus Esteban Díez <56318729+Jesusestebandiez@users.noreply.github.com>
2022-09-10 17:57:47 +0200

    Add files via upload

--- Pokemon/Pokeball.js
+++ Pokemon/Pokeball.js
@@ -1,12 +1,11 @@
 
 'use strict'
 
-//con esto enlazo el html con la js 
-
 const pokelist = document.querySelector(".pokeball");
 
-const getpokemons = async () => { //De está forma obtenemos los pokemon
-    const pokemons =[] ;
+const getpokemons = async () => {
+    //De está forma obtenemos los pokemon
+    const pokemons = [] ;
     for (let index =1; index < 152; index++) {
         let pokemonsfetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
         let datospokemon = await pokemonsfetch.json();
@@ -15,49 +14,48 @@ const getpokemons = async () => { //De está forma obtenemos los pokemon
 
     }
 
-    const pokemap = pokemons.map((element) => ({
-        id:element.id,
-        name:element.name,
-        image:element.sprites.other["official-artwork"] ["front_default"],
-        status:element.stats[1] ["base_stat"],
-        status2:element.stats[2] ["base_stat"],
-    }));
+    const pokemap = pokemons.map(element => {
+        const gender = element.sprites.back_female ? "Masculino" : "Femenino";
+        let response = {
+            id: element.id,
+            name: element.name,
+            image: element.sprites.other["official-artwork"] ["front_default"],
+            status: element.stats[1] ["base_stat"],
+            status2: element.stats[2] ["base_stat"],
+            height: element.height,
+            types: element.types,
+            abilities: element.abilities,
+            weight: element.weight,
+            gender: gender
+        }
+        return response
+    });
 
     return pokemap;
 }
 
 const drawpokemons = async () => {
-    const pdatos = ["altura", "categoria", "habilidad", "sexo", "peso"];
-
-    const poketext =[] ; 
-for (let i = 0; i < pdatos.length; i++) {
-  let text = pdatos[i];  
-  poketext.push(text); 
-  console.log(poketext);
-}
     const pokemons = await getpokemons();
     pokemons.forEach(element => {
-        pokelist.innerHTML += 
-            '<li>' +
-                '<h3>' + element.name + '</h3>' +
-                '<img  src=' + element.image + ' ></img>' +
-            '</li>';
+        const abilities = element.abilities;
+        const types = element.types;
+        const abilitiesElements = abilities.map(
+            element => '<div style="text-transform: capitalize">' + element.ability.name + '</div>').join('');
+        const typesElements = types.map(
+            element => '<div style="text-transform: capitalize">' + element.type.name + '</div>').join('');
+        pokelist.innerHTML +=
+            '<li class="card">' +
+                '<h3 style="text-transform: capitalize">' + element.name + '</h3>' +
+                '<div class="flip-card" ><div class="flip-card-inner">' +
+                '<div class="flip-card-front"><img  src=' + element.image + ' ></img></div>' +
+                '<div class="flip-card-back">' + 
+                '<div>Altura: ' + element.height + '</div>' +
+                '<div>Habilidades: ' + abilitiesElements + '</div>' +
+                '<div>Peso: ' + element.weight + '</div>' +
+                '<div>Género: ' + element.gender + '</div>' +
+                '<div>Categorías: ' + typesElements + '</div>' +
+            '</div></div></div></li>';
     });
-    
-
- //mostrar la info
-
- const pokedatos = document.querySelector(".pokedatos") ;
- '<div class= "flip_Card">'
- '</div>'
- '<div class= "flip_Card-inner">'
- '</div>'
-'<div class="flip-card-front">'
-'</div>'
-'<div class="flip-card-back">'+'<p class="pokedatos"></p>'+'</div>'
- pokedatos.innerHTML= poketext;
-
 }
 
 drawpokemons();
-
